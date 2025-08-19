09690d2cb55e1d4e627ddb44a30f2e0e273d331f
quickresumeedit <support@quickresumeedit.com>
2025-08-19 01:43:35 -0400

    Update create-checkout-session.ts

--- pages/api/create-checkout-session.ts
+++ pages/api/create-checkout-session.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import Stripe from 'stripe';
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '', { apiVersion: '2024-06-20' });
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '', { apiVersion: '2023-10-16' });
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'POST') return res.status(405).json({ error: 'Method not allowed' });
